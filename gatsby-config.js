b6dee04b7ec0d5edf276fd6727a7e9baf409a65f
Saul Corona <saulcoronasantos@ymail.com>
2020-07-11 20:58:37 -0500

    add FP $ GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,35 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-75589601-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: true,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Defers execution of google analytics script after page load
+        defer: false,
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "saulcorona.com",
+      },
+    },
+    {
+      resolve: `gatsby-plugin-facebook-pixel`,
+      options: {
+        pixelId: '150833485734338',
+      },
+    },
+    {
      resolve:`gatsby-plugin-sitemap`,
     },
     {
